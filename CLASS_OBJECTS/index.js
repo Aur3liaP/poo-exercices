6c6d364bb2ab0808385cdcbb8c8a7f221f889899
Pic Aurélia <pic.aurelia@gmail.com>
2025-02-06 11:36:59 +0100

    Create Class & Object exercice done

--- CLASS_OBJECTS/index.js
+++ CLASS_OBJECTS/index.js
@@ -9,3 +9,19 @@
 // Ajoutez un constructeur qui initialise les propriétés title, author, et year.
 // Ajoutez une méthode getSummary qui retourne un résumé du livre.
 // Créez plusieurs instances de la classe Book et affichez leur résumé.
+
+class Book {
+    constructor (title, author, year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    getSummary(){
+        return `${this.title} was written by ${this.author} in ${this.year}`
+    }
+}
+let harryP = new Book("Harry Potter", "JK Rowling", 1997);
+let sDA = new Book("Le Seigneur des Anneaux", "J.R.R Tolkien", 1954)
+
+console.log(harryP.getSummary());
+console.log(sDA.getSummary());
